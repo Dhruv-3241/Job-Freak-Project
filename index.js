00c0c70d5b8f5e9117410e2e341d13978663b5b9
Dhruv Srivastava <138190758+Dhruv-3241@users.noreply.github.com>
2023-07-01 02:12:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var btn1 = document.getElementById("b1");
+var btn2 = document.getElementById("b2");
+var btn3 = document.getElementById("b3");
+var btn4 = document.getElementById("b4");
+
+btn1.addEventListener("click", (e) => {
+    console.log("1");
+});
+
+btn2.addEventListener("click", (e) => {
+    console.log("2");
+});
+
+btn3.addEventListener("click", (e) => {
+    console.log("3");
+});
+
+btn4.addEventListener("click", (e) => {
+    console.log("4");
+});
